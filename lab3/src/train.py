587af4984e9adda2dd6317f48935d48ac4dadc61
yeeecheng <yichengliao1003@gmail.com>
2024-07-27 14:34:26 +0800

    add transform data augmentation

--- lab3/src/train.py
+++ lab3/src/train.py
@@ -15,10 +15,9 @@ def train(args):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
     train_transforms = transforms.Compose([
-        transforms.Grayscale(num_output_channels=3),
-        transforms.ElasticTransform(),
+        transforms.ColorJitter(brightness=0.4),
+        transforms.ElasticTransform(alpha=float(250), sigma=float(10)),
         transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(degrees= 15),
     ])
 
     train_dataset = SimpleOxfordPetDataset(args.data_path, mode= "train", transform= train_transforms)
@@ -28,6 +27,9 @@ def train(args):
     BATCH_SIZE = args.batch_size
     LR = args.learning_rate
     NET = args.net
+    PRE_TRAIN_MODEL = args.model 
+    
+
     train_dataloader = DataLoader(train_dataset, BATCH_SIZE, shuffle= True)
     val_dataloader = DataLoader(val_dataset, BATCH_SIZE, shuffle= False)    
 
@@ -41,11 +43,21 @@ def train(args):
         criterion = nn.CrossEntropyLoss()
         # lr of 1e-3, batch size of 6
         optimizer = torch.optim.SGD(model.parameters(), lr= LR)
+
+    prev_epochs = 0
+    if PRE_TRAIN_MODEL is not None:
+        checkpoint = torch.load(PRE_TRAIN_MODEL)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        prev_epochs = checkpoint['epoch']
+    
+    EPOCHS += prev_epochs
+    
     best_acc = 0
     history = {"train_loss": [], "train_acc": [], "val_loss": [], "val_acc": []}
 
-
-    for epoch in range(EPOCHS):
+    
+    for epoch in range(prev_epochs + 1, EPOCHS + 1):
         
         model.train()
         batch_train_loss = []
@@ -59,13 +71,13 @@ def train(args):
 
             masks_pred = model(imgs.to(device))
 
-            loss = criterion(masks_pred, masks.to(device))
+            acc = dice_score(torch.argmax(masks_pred, dim=1),masks.to(device))
+            loss = criterion(masks_pred, masks.to(device)) + (1 - acc)
             
             torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
             loss.backward()
             optimizer.step()
 
-            acc = dice_score(torch.argmax(masks_pred, dim=1),masks.to(device))
 
             batch_train_acc.append(acc)
             batch_train_loss.append(loss.item())
@@ -81,11 +93,11 @@ def train(args):
         history["val_acc"].append(val_acc)
         history["val_loss"].append(val_loss)
 
-        print(f"[ Train | {epoch + 1:03d}/{EPOCHS:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f} [ Validation | {epoch + 1:03d}/{EPOCHS:03d} ] val_loss = {val_loss:.5f}, val_acc = {val_acc:.5f}")
+        print(f"[ Train | {epoch:03d}/{EPOCHS:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f} [ Validation | {epoch:03d}/{EPOCHS:03d} ] val_loss = {val_loss:.5f}, val_acc = {val_acc:.5f}")
 
         if val_acc > best_acc:
             print(f"Best model found at epoch {epoch}, saving model")
-            torch.save(model.state_dict(), "./best.ckpt")
+            torch.save({'model_state_dict': model.state_dict(), 'epoch': epoch, 'optimizer_state_dict': optimizer.state_dict()}, "./best.pth")
             best_acc = val_acc
 
     # draw model history 
@@ -98,6 +110,7 @@ def get_args():
     parser.add_argument('--batch_size', '-b', type=int, default=1, help='batch size')
     parser.add_argument('--learning-rate', '-lr', type=float, default=1e-5, help='learning rate')
     parser.add_argument('--net', '-n', type=str, default="UNet")
+    parser.add_argument('--model', default= None, help='path to the stored model weight')
 
     return parser.parse_args()
 
